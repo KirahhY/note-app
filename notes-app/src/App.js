39285ba82090d413212ab1dea203cb8c5b0ab9e6
KirahhY <marius.oudin@gmail.com>
2024-01-21 20:40:24 +0100

    noteCount added for AddNote function

--- notes-app/src/App.js
+++ notes-app/src/App.js
@@ -3,29 +3,32 @@ import "./App.css";
 
 export default function App() {
   const [notes, setNotes] = useState(null);
-  async function fetchNotes() {
-    const response = await fetch("/notes");
-    const data = await response.json();
-    setNotes(data.reverse());
-  }
 
+  // save l'array notes (id, title, content) dans data 
+  async function fetchNotes() {
+    const response = await fetch("/notes"); // get par défaut
+    const data = await response.json();
+    setNotes(data);
+  }
+  
+
+  const [noteCount, setNoteCount] = useState(1)
   async function AddNote() {
     const newNote = {
-      title: "Nouvelle note",
+      title: "Note " + noteCount, 
       content: "note",
     };
-
+    setNoteCount(prevNoteCount => prevNoteCount + 1)
     const requestOptions = {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(newNote),
+      method: "POST", // à voir => utiliser patch (modif) ou get (get par défaut)
+      headers: { "Content-Type": "application/json" }, // à mettre dans toutes les requêtes
+      body: JSON.stringify(newNote), // pareil
     };
-
     const response = await fetch("/notes", requestOptions);
-
     fetchNotes();
   };
 
+  // s'exécute au début du programme
   useEffect(function () {
     fetchNotes();
   }, []);
