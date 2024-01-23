728d39b71288cb5e9529b310eadfe1081b5956cb
KirahhY <marius.oudin@gmail.com>
2024-01-23 19:17:23 +0100

    cleanning App.js after migrating code to Home.js

--- notes-app/src/App.js
+++ notes-app/src/App.js
@@ -1,7 +1,4 @@
-import { useState, useEffect, useTransition } from "react";
 import "./App.css";
-import Sidebar from "./components/Sidebar";
-import NoteContent from "./components/NoteContent";
 import Navbar from "./components/Navbar";
 import { BrowserRouter, Link, Navigate, Route, Routes } from "react-router-dom";
 import Home from "./views/Home";
@@ -30,32 +27,9 @@ export default function App() {
             path="/notes/:id"
             element={<Home />}
           />
-
           <Route path="*" element={<Navigate to="/" />} />
         </Routes>
       </main>
     </BrowserRouter>
-
-    // <div>
-    //   <Navbar />
-    //   <div className="container">
-    //     <Sidebar 
-    //       className="Sidebar"
-    //       notes={notes} 
-    //       AddNote={AddNote}
-    //       selectNote={selectNote}
-    //       selectedNote={selectedNote}
-    //       noteChecked={noteChecked}
-    //       handleChange={handleNoteCheck}
-    //     />
-    //     <NoteContent
-    //       className="NoteContent" 
-    //       notes={notes}
-    //       AddNote={AddNote} 
-    //       selectedNote={selectedNote}
-    //       handleTitleChange={handleTitleChange}
-    //     />
-    //   </div>
-    // </div>
   );
 };
