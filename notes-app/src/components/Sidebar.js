19c7415431d0b3e67ee7330d9bf1e34c3ff36f9f
KirahhY <marius.oudin@gmail.com>
2024-01-24 19:15:57 +0100

    Adding trash icon for DeleteNote function

--- notes-app/src/components/Sidebar.js
+++ notes-app/src/components/Sidebar.js
@@ -1,31 +1,31 @@
 import AddButton from "../images/newNote.png"
+import Bin from "../images/trash-bin.png"
 
 export default function Sidebar(props){
     return(
         <aside className="Side">
-            <button className="Button-create-note" onClick={props.AddNote}>
-                <img src={AddButton} alt="add note logo" width="30px"/>
-            </button>
+            <div className="sidebar-icons">
+                <button className="Button-create-note" onClick={props.AddNote}>
+                    <img src={AddButton} alt="add note logo" width="40px"/>
+                </button>
+                <button className="Button-create-note" onClick={props}>
+                    <img src={Bin} alt="add note logo" width="35px"/>
+                </button>
+            </div>
 
             {props.notes !== null
             ? props.notes.map((note) => (
-                <div
-                    // className={`title ${
-                    //     note.id === props.selectedNote.id ? "selected-note" : ""
-                    // }`} 
-                    // buggé
+                <div className="Side-title-input-container"
                     key={note.id}
                     onClick={() => props.selectNote(note)}>
-                        <div className="Side-title-input-container">
-                            <h1>{note.title}</h1>
-                            <input 
-                                type="checkbox"  
-                                // checked={note.checked || false}
-                                checked={note.checked}
-                                onChange={() => props.handleNoteChange(note.id)}
-                                name="noteChecked"
-                            ></input>
-                        </div>
+                        <h1>{note.title}</h1>
+                        <input 
+                            type="checkbox"  
+                            // checked={note.checked || false}
+                            checked={note.checked}
+                            onChange={() => props.handleNoteChange(note.id)}
+                            name="noteChecked"
+                        ></input>
                 </div>
                 ))
             : null}
